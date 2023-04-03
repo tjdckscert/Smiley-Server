d78190c8a324aa71e15240620addb7c6fe762e68
tjdckscert <tjdcks1051@naver.com>
2023-04-03 20:35:51 +0900

    fix : 코드리뷰 요청사항 반영완료.(#24)

--- src/main/java/com/smiley/smileybackend/controller/MedicineController.java
+++ src/main/java/com/smiley/smileybackend/controller/MedicineController.java
@@ -1,18 +1,13 @@
 package com.smiley.smileybackend.controller;
 
 import com.smiley.smileybackend.dto.response.MedicineInfoDto;
-import com.smiley.smileybackend.dto.user.MedicineIdDto;
 import com.smiley.smileybackend.service.MedicineService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
+import org.springframework.web.bind.annotation.*;
 
 @ToString
 @RestController
@@ -25,10 +20,10 @@ public class MedicineController {
         this.medicineService = medicineService;
     }
 
-    @PostMapping("/medicine")
+    @GetMapping("/medicines/{id}")
     @ApiOperation(value="의약품 정보 조회" , notes = "Id 값을 통해 단일 의약품 정보를 가져온다")
-    public ResponseEntity<MedicineInfoDto> medicine(@Valid @RequestBody MedicineIdDto medicineIdDto) {
-        MedicineInfoDto medicineInfoDto = medicineService.findMedicine(medicineIdDto);
+    public ResponseEntity<MedicineInfoDto> medicine(@PathVariable Integer id) {
+        MedicineInfoDto medicineInfoDto = medicineService.findMedicine(id);
         log.info(medicineInfoDto.toString());
         return ResponseEntity.ok(medicineInfoDto);
     }
--- src/main/java/com/smiley/smileybackend/service/MedicineService.java
+++ src/main/java/com/smiley/smileybackend/service/MedicineService.java
@@ -2,12 +2,9 @@ package com.smiley.smileybackend.service;
 
 import com.smiley.smileybackend.domain.Medicine;
 import com.smiley.smileybackend.dto.response.MedicineInfoDto;
-import com.smiley.smileybackend.dto.user.MedicineIdDto;
 import com.smiley.smileybackend.repository.MedicineRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
-import javax.validation.Valid;
 
 @Service
 @Slf4j
@@ -19,8 +16,8 @@ public class MedicineService {
     /**
      * ID 값을 통해 단일 의약품 정보를 가져온다
      * */
-    public MedicineInfoDto findMedicine(@Valid MedicineIdDto medicineIdDto) {
-        Medicine medicine = medicineRepository.findById(medicineIdDto.getId()).orElseThrow(
+    public MedicineInfoDto findMedicine(Integer id) {
+        Medicine medicine = medicineRepository.findById(id).orElseThrow(
                 () -> new IllegalArgumentException("의약품을 찾을 수 없습니다")
         );
         return new MedicineInfoDto(medicine);
