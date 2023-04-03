951f54c6c570cb020256d227f320bf33249c1d1b
tjdckscert <115801420+tjdckscert@users.noreply.github.com>
2023-04-03 20:39:22 +0900

    [Feat] : 단일 의약품 정보 조회(#22) (#24)
    * feat : 사용자 의료정보 저장기능 구현완료 (#20)
    
    * feat : UserController 추가 완료(#20)
    
    * refactor : 코드리뷰 요청사항 반영완료(#21)
    
    * fix : 코드리뷰 요청사항 반영완료.orElseThrow Exception 추가 완료(#21)
    
    * feat :MedicineService, MedidcineRepository, MedicineController, MedicineDto 생성완료(#22)
    
    * fix : 코드리뷰 요청사항 반영완료.(#24)

---
+++ src/main/java/com/smiley/smileybackend/service/MedicineService.java
@@ -0,0 +1,25 @@
+package com.smiley.smileybackend.service;
+
+import com.smiley.smileybackend.domain.Medicine;
+import com.smiley.smileybackend.dto.response.MedicineInfoDto;
+import com.smiley.smileybackend.repository.MedicineRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class MedicineService {
+    private MedicineRepository medicineRepository;
+    public MedicineService(MedicineRepository medicineRepository){
+        this.medicineRepository = medicineRepository;
+    }
+    /**
+     * ID 값을 통해 단일 의약품 정보를 가져온다
+     * */
+    public MedicineInfoDto findMedicine(Integer id) {
+        Medicine medicine = medicineRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("의약품을 찾을 수 없습니다")
+        );
+        return new MedicineInfoDto(medicine);
+    }
+}
