65154ec2326f8bf417d9cf49231a275927d9d308
tjdckscert <tjdcks1051@naver.com>
2023-04-06 01:56:18 +0900

    feat : MagazineService Add Function(#40)

--- src/main/java/com/smiley/smileybackend/service/MagazineService.java
+++ src/main/java/com/smiley/smileybackend/service/MagazineService.java
@@ -21,4 +21,11 @@ public class MagazineService {
         return magazineRepository.findAll().stream().map(MagazineInfoDto::entityToDto).collect(Collectors.toList());
     }
 
+    /**
+     * 선택한 수량만큼의 매거진 정보를 가져온다
+     * */
+    public List<MagazineInfoDto> getListOfNumber(Integer number) {
+        return magazineRepository.findNumberOfMagazines(number).stream().map(MagazineInfoDto::entityToDto).collect(Collectors.toList());
+    }
+
 }
