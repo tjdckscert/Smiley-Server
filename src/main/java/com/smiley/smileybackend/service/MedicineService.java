b86a836f3eeee20fe548821ff57684acc77ad84a
tjdckscert <tjdcks1051@naver.com>
2023-04-10 20:40:09 +0900

    refator : Change Medicine itemCode Integer->String (#48)

--- src/main/java/com/smiley/smileybackend/service/MedicineService.java
+++ src/main/java/com/smiley/smileybackend/service/MedicineService.java
@@ -20,8 +20,8 @@ public class MedicineService {
     /**
      * ID 값을 통해 단일 의약품 정보를 가져온다
      * */
-    public MedicineInfoDto findMedicine(Integer id) {
-        Medicine medicine = medicineRepository.findById(id).orElseThrow(
+    public MedicineInfoDto findMedicine(String itemCode) {
+        Medicine medicine = medicineRepository.findById(itemCode).orElseThrow(
                 () -> new IllegalArgumentException("의약품을 찾을 수 없습니다")
         );
         return new MedicineInfoDto(medicine);
