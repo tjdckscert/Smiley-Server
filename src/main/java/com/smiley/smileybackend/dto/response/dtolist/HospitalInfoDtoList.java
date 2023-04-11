3cf3e644f125468353231200164fd409b7b8207d
tjdckscert <tjdcks1051@naver.com>
2023-04-11 13:27:56 +0900

    feat : Make Response Entity(#50)

--- src/main/java/com/smiley/smileybackend/dto/response/dtolist/HospitalInfoDtoList.java
+++ src/main/java/com/smiley/smileybackend/dto/response/dtolist/HospitalInfoDtoList.java
@@ -1,2 +1,18 @@
-package com.smiley.smileybackend.dto.response.dtolist;public class HospitalInfoDtoList {
+package com.smiley.smileybackend.dto.response.dtolist;
+
+import com.smiley.smileybackend.dto.response.HospitalInfoDto;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+@Getter
+@NoArgsConstructor
+public class HospitalInfoDtoList {
+    List<HospitalInfoDto> hospitals;
+
+    @Builder
+    public HospitalInfoDtoList(List<HospitalInfoDto> hospitals) {
+        this.hospitals = hospitals;
+    }
 }
