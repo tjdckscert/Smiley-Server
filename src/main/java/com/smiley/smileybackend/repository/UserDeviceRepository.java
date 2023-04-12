8822776d8713307b8ea28ac913bd62327705cb63
tjdckscert <tjdcks1051@naver.com>
2023-04-12 19:04:40 +0900

    Add UserDeviceRepository(#52)

--- src/main/java/com/smiley/smileybackend/repository/UserDeviceRepository.java
+++ src/main/java/com/smiley/smileybackend/repository/UserDeviceRepository.java
@@ -1,2 +1,14 @@
-package com.smiley.smileybackend.repository;public interface UserDeviceRepository {
+package com.smiley.smileybackend.repository;
+
+
+import com.smiley.smileybackend.domain.UserDevice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserDeviceRepository extends JpaRepository<UserDevice,Integer> {
+
+    Optional<UserDevice> findByDeviceUuid(String deviceUuid);
 }
