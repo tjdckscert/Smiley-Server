f77e50826f124b5c1f65c58361d23c8670014949
rlawns0327 <rlawns0327@naver.com>
2023-05-02 20:53:13 +0900

    feat : 이미지 파일 userid 형태로 저장 / 기존 파일 존재 시 삭제 구현 (#58)

--- src/main/java/com/smiley/smileybackend/service/ToothBrushStatusService.java
+++ src/main/java/com/smiley/smileybackend/service/ToothBrushStatusService.java
@@ -22,14 +22,51 @@ public class ToothBrushStatusService {
 
 
 
+    // 1. 요청 들어온 사진 서버에 저장하기
     public void saveFile(String userId, MultipartFile imageFile) throws IOException {
 
         log.info("들어온 사진 이름 확인: " + imageFile.getOriginalFilename());
 
+        // 해당 경로에 USER ID를 이름으로 이미지 저장
         String saveFileName = requestPath + userId + ".jpg";
+        imageFile.transferTo(new File(saveFileName));   // 사진 저장
 
-        imageFile.transferTo(new File(saveFileName));
-        //deleteOriginalFile();
-        //runPython();
     }
+
+    // 2. 기존에 저장되어 있는 파일 삭제 ( 폴더가 아이디 이름으로 되어 있음 ) -> YOLO가 동일 이름으로 들어오면 이름을 바꿔버림
+    public void deleteOriginalFile(String userId){
+        File file = new File("C:\\Users\\joon\\Desktop\\ToothFairy_Server\\src\\main\\python\\toothbrush\\yolov5\\runs\\detect\\"+userId);
+
+        if(!file.exists()){ // 기존에 저장된 파일이 없다면
+            log.info("삭제할 파일이 존재하지 않습니다.");
+        }
+        else // 기존에 저장된 파일이 있다면 삭제 처리
+        {
+            if(file.isDirectory()) { //파일이 디렉토리인지 확인 -> 디렉토리가 비워있지 않다면 삭제가 안됨
+                log.info("기존 파일 존재 - 삭제 진행");
+                File[] files = file.listFiles();
+                for( int i=0; i<files.length; i++){ // 디렉터리에 있는 내부 파일들을 모두 삭제
+                    if( files[i].delete() ){
+                        log.info("폴더 내부 파일 삭제 성공");
+                    }else{
+                        log.info("폴더 내부 파일 삭제 실패");
+                    }
+                }
+            }
+            if(file.delete()){
+                log.info("파일삭제 성공");
+            }else{
+                log.info("파일삭제 실패");
+            }
+        }
+
+    }
+
+    // 3. 이미지 학습 시키기
+
+    // 4. 학습된 파일 가져오기
+
+
+
+
 }
