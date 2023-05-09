760f69ca28b9a2cd84918c4d0d67230b014e916d
tjdckscert <tjdcks1051@naver.com>
2023-05-09 23:04:52 +0900

    feat : Add HospitalRepository Function(#62)

--- src/main/java/com/smiley/smileybackend/repository/HospitalRepository.java
+++ src/main/java/com/smiley/smileybackend/repository/HospitalRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,4 +15,12 @@ public interface HospitalRepository extends JpaRepository<Hospital, Hospitalkeys
 
     @Query("select h from Hospital h where h.hPid = :hPid")
     Optional<Hospital> findByHPid(@Param("hPid") String hPid);
+    @Query(nativeQuery = true,value = "SELECT *,\n" +
+            "(6371*acos(cos(radians( :userlan))*cos(radians(wgs84lat))*cos(radians(wgs84lon) \n" +
+            "-radians( :userlon))+sin(radians( :userlan))*sin(radians(wgs84lat)))) \n" +
+            "AS distance \n" +
+            "FROM hospital \n" +
+            "HAVING distance <= :distance \n" +
+            "ORDER BY distance ;")
+    List<Hospital> findNearHospitals(@Param("distance") Double distance, @Param("userlan") Double  userlan, @Param("userlon") Double  userlon);
 }
