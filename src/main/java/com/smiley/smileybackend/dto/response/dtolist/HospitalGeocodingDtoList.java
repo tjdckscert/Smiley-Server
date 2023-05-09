006eef193c42b27a9ad27dad3060222e480add3c
tjdckscert <tjdcks1051@naver.com>
2023-05-09 23:56:14 +0900

    feat : Add New ResponseDtoList (#64)

--- src/main/java/com/smiley/smileybackend/dto/response/dtolist/HospitalGeocodingDtoList.java
+++ src/main/java/com/smiley/smileybackend/dto/response/dtolist/HospitalGeocodingDtoList.java
@@ -1,2 +1,21 @@
-package com.smiley.smileybackend.dto.response.dtolist;public class HospitalGeocodingDtoList {
+package com.smiley.smileybackend.dto.response.dtolist;
+
+import com.smiley.smileybackend.dto.response.HospitalGeocodingDto;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@ToString
+public class HospitalGeocodingDtoList {
+    List<HospitalGeocodingDto> hospitals;
+
+    @Builder
+    public HospitalGeocodingDtoList(List<HospitalGeocodingDto> hospitals) {
+        this.hospitals = hospitals;
+    }
 }
