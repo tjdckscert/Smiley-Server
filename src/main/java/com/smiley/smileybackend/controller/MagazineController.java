cb8b04118cf61fa9b1d08bb4c00dca4d5dcf7f2c
tjdckscert <tjdcks1051@naver.com>
2023-05-11 18:43:13 +0900

    refactor : premier function(#66)

--- src/main/java/com/smiley/smileybackend/controller/MagazineController.java
+++ src/main/java/com/smiley/smileybackend/controller/MagazineController.java
@@ -26,13 +26,13 @@ public class MagazineController {
         return ResponseEntity.ok(magazines);
     }
 
-    @GetMapping("/magazines/{number}") //쿼리 파라미터로 변경
-    @ApiImplicitParam(name="number",value="가져올 매거진의 수량",example = "1")
-    @ApiOperation(value="선택수량 매거진" , notes = "선택한 수량만큼의 매거진 정보를 반환한다.")
-    public ResponseEntity<MagazineInfoDtoList> numberOfMagazines(@PathVariable Integer number){
-        MagazineInfoDtoList magazines = magazineService.getListOfNumber(number);
-        return ResponseEntity.ok(magazines);
-    }
+//    @GetMapping("/magazines/{number}") //쿼리 파라미터로 변경
+//    @ApiImplicitParam(name="number",value="가져올 매거진의 수량",example = "1")
+//    @ApiOperation(value="선택수량 매거진" , notes = "선택한 수량만큼의 매거진 정보를 반환한다.")
+//    public ResponseEntity<MagazineInfoDtoList> numberOfMagazines(@PathVariable Integer number){
+//        MagazineInfoDtoList magazines = magazineService.getListOfNumber(number);
+//        return ResponseEntity.ok(magazines);
+//    }
 
     @GetMapping("/magazines/{number}")
     @ApiOperation(value="선택한 메저긴 정보 불러오기" , notes = "선택한 메거진의 정보를 불러와서 반환한다. 사진은 서버에 저장된 사진을 반환.")
