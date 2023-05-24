aa56ddf69c5f923a54a96e7e1b15ee563703a7b1
tjdckscert <tjdcks1051@naver.com>
2023-05-24 21:36:02 +0900

    feat : Add List<userMedicalInfo> variable & Add entityToDto() Function(#80)

--- src/main/java/com/smiley/smileybackend/dto/response/UserInfoDto.java
+++ src/main/java/com/smiley/smileybackend/dto/response/UserInfoDto.java
@@ -1,6 +1,7 @@
 package com.smiley.smileybackend.dto.response;
 
 import com.smiley.smileybackend.domain.User;
+import com.smiley.smileybackend.domain.UserMedicalInfo;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Data;
@@ -8,6 +9,7 @@ import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Data
 @Slf4j
@@ -28,12 +30,29 @@ public class UserInfoDto {
     @ApiModelProperty( example = "사용자 생일")
     private LocalDate birthDate;
 
+    @ApiModelProperty( example = "사용자 의료정보")
+    private List<UserMedicalInfo> userMedicalInfos;
+
+
     @Builder
-    public UserInfoDto(User saved) {
-        this.id = saved.getId();
-        this.userNumber = saved.getUserNumber();
-        this.phoneToken = saved.getPhoneToken();
-        this.name = saved.getName();
-        this.birthDate = saved.getBirthDate();
+    public UserInfoDto(Integer id, String userNumber, String phoneToken, String name, LocalDate birthDate, List<UserMedicalInfo> userMedicalInfos) {
+        this.id = id;
+        this.userNumber = userNumber;
+        this.phoneToken = phoneToken;
+        this.name = name;
+        this.birthDate = birthDate;
+        this.userMedicalInfos = userMedicalInfos;
+    }
+
+    public UserInfoDto entityToDto(User user) {
+        return new UserInfoDto(
+                user.getId(),
+                user.getUserNumber(),
+                user.getPhoneToken(),
+                user.getName(),
+                user.getBirthDate(),
+                user.getUserUserMedicalInfos()
+        );
+
     }
 }
