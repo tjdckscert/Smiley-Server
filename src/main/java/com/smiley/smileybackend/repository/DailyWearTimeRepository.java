7bc48bdf1dc7df3b40cc855834fa5e971f230d37
tjdckscert <tjdcks1051@naver.com>
2023-05-24 22:24:32 +0900

    feat : Add DailyWearTimeRepository Add Function(#82)

--- src/main/java/com/smiley/smileybackend/repository/DailyWearTimeRepository.java
+++ src/main/java/com/smiley/smileybackend/repository/DailyWearTimeRepository.java
@@ -1,9 +1,14 @@
 package com.smiley.smileybackend.repository;
 
 import com.smiley.smileybackend.domain.DailyWearTime;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Repository
 public interface DailyWearTimeRepository extends JpaRepository<DailyWearTime,Integer> {
+    List<DailyWearTime> findByUserIdOrderByIdDesc(Integer id, Pageable pageable);
 }
