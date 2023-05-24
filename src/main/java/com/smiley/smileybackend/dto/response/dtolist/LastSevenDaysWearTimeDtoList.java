52d6a072b54dafc7d10f4d6c4beeba7210bef004
tjdckscert <tjdcks1051@naver.com>
2023-05-24 22:24:15 +0900

    feat : Add ResponseDto(#82)

--- src/main/java/com/smiley/smileybackend/dto/response/dtolist/LastSevenDaysWearTimeDtoList.java
+++ src/main/java/com/smiley/smileybackend/dto/response/dtolist/LastSevenDaysWearTimeDtoList.java
@@ -1,2 +1,21 @@
-package com.smiley.smileybackend.dto.response.dtolist;public class LastSevenDaysWearTimeDtoList {
+package com.smiley.smileybackend.dto.response.dtolist;
+
+import com.smiley.smileybackend.dto.response.LastSevenDaysWearTimeDto;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@ToString
+public class LastSevenDaysWearTimeDtoList {
+    List<LastSevenDaysWearTimeDto> weartimes;
+
+    @Builder
+    public LastSevenDaysWearTimeDtoList(List<LastSevenDaysWearTimeDto> weartimes) {
+        this.weartimes = weartimes;
+    }
 }
