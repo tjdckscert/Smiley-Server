025e9470732d163eb14d291c4589d94952e8b6c4
tjdckscert <tjdcks1051@naver.com>
2023-05-31 02:30:33 +0900

    refactor : Edit UserDeviceService Exception(#89)

--- src/main/java/com/smiley/smileybackend/exception/ErrorCode.java
+++ src/main/java/com/smiley/smileybackend/exception/ErrorCode.java
@@ -10,6 +10,7 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 public enum ErrorCode {
     /* 404 NOT_FOUND : Resource 를 찾을 수 없음 */
     MAGAZINE_NOT_FOUND(NOT_FOUND, "메거진 정보를 찾을 수 없습니다"),
+    DEVICE_ALREADY_REGISTER(NOT_FOUND, "디바이스가 이미 등록되어 있습니다."),
     PICTURE_NOT_FOUND(NOT_FOUND, " 사진을 찾을 수 없습니다"),
     HOSPITAL_NOT_FOUND(NOT_FOUND, "병원 정보를 찾을 수 없습니다"),
     USER_NOT_FOUND(NOT_FOUND, "사용자를 찾을 수 없습니다"),
