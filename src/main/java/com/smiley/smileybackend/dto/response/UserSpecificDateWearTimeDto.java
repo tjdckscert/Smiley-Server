0c2550879e7c2b73bb4a5cbfc003b6b52420dd10
tjdckscert <tjdcks1051@naver.com>
2023-06-08 19:20:48 +0900

    feat : Add ResponseDto(#83)

--- src/main/java/com/smiley/smileybackend/dto/response/UserSpecificDateWearTimeDto.java
+++ src/main/java/com/smiley/smileybackend/dto/response/UserSpecificDateWearTimeDto.java
@@ -1,2 +1,26 @@
-package com.smiley.smileybackend.dto.response;public class UserSpecificDateWearTimeDto {
+package com.smiley.smileybackend.dto.response;
+
+import com.smiley.smileybackend.domain.DailyWearTime;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class UserSpecificDateWearTimeDto {
+
+    private String userNumber;
+
+    private LocalDate wearDate;
+
+    private Integer totalWearTime;
+
+    @Builder
+    public UserSpecificDateWearTimeDto(DailyWearTime saved) {
+        this.userNumber =saved.getUser().getUserNumber();
+        this.wearDate = saved.getWearDate();
+        this.totalWearTime=saved.getTotalWearTime();
+    }
 }
