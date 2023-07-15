7029e746119fc99eebe3c9ff401c3509ad465db9
tjdckscert <tjdcks1051@naver.com>
2023-07-16 01:21:00 +0900

    refactor : Edit MagazineService Function(#110)

--- src/main/java/com/smiley/smileybackend/service/MagazineService.java
+++ src/main/java/com/smiley/smileybackend/service/MagazineService.java
@@ -1,12 +1,9 @@
 package com.smiley.smileybackend.service;
 
 import com.smiley.smileybackend.domain.Magazine;
-import com.smiley.smileybackend.domain.enums.ContentType;
 import com.smiley.smileybackend.dto.response.MagazineDetailDto;
 import com.smiley.smileybackend.dto.response.MagazineInfoDto;
 import com.smiley.smileybackend.dto.response.dtolist.MagazineInfoDtoList;
-import com.smiley.smileybackend.dto.user.ContentImgJsonDto;
-import com.smiley.smileybackend.dto.user.ContentLinkJsonDto;
 import com.smiley.smileybackend.exception.ErrorCode;
 import com.smiley.smileybackend.exception.ErrorException;
 import com.smiley.smileybackend.repository.MagazineRepository;
@@ -16,8 +13,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
@@ -70,27 +65,19 @@ public class MagazineService {
      */
     public MagazineDetailDto getMagazineDetail(Integer number) {
         Magazine magazine = magazineRepository.findById(number).orElseThrow(
-                ()->new ErrorException(ErrorCode.MAGAZINE_NOT_FOUND)
+                () -> new ErrorException(ErrorCode.MAGAZINE_NOT_FOUND)
         );
-        List<ContentImgJsonDto> list = new ArrayList<>();
-        for (ContentLinkJsonDto c : magazine.getMainContent()){
-            ContentImgJsonDto contentImgJsonDto = new ContentImgJsonDto();
-            if (c.getContentType()==ContentType.MAIN_CONTENT){
-                try {
-                    InputStream inputStream = new FileInputStream(c.getImgLink());
-                    long fileSize = new File(c.getImgLink()).length();
-                    contentImgJsonDto.setImg(new byte[(int) fileSize]);
-                    while (inputStream.read(contentImgJsonDto.getImg())>0);
-                    inputStream.close();
-                } catch (IOException e) {
-                    throw new ErrorException(ErrorCode.PICTURE_NOT_FOUND);
-                }
-            }
-            contentImgJsonDto.setContentType(c.getContentType());
-            contentImgJsonDto.setContent(c.getContent());
-            list.add(contentImgJsonDto);
+        byte[] img;
+        try {
+            InputStream inputStream = new FileInputStream(magazine.getThumbnail());
+            long fileSize = new File(magazine.getThumbnail()).length();
+            img = new byte[(int) fileSize];
+            while (inputStream.read(img) > 0) ;
+            inputStream.close();
+        } catch (IOException e) {
+            throw new ErrorException(ErrorCode.PICTURE_NOT_FOUND);
         }
-        return new MagazineDetailDto(magazine,list);
+        return new MagazineDetailDto(magazine,img);
     }
 
 }
