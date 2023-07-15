9ccd0392fcc1b53b1b65bb4ffe5deb142b29b1d2
tjdckscert <tjdcks1051@naver.com>
2023-07-16 01:24:27 +0900

    refactor : Delete Magazine Json type(#110)

--- src/main/java/com/smiley/smileybackend/domain/Magazine.java
+++ src/main/java/com/smiley/smileybackend/domain/Magazine.java
@@ -1,10 +1,8 @@
 package com.smiley.smileybackend.domain;
 
-import com.vladmihalcea.hibernate.type.json.JsonType;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.TypeDef;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -12,9 +10,7 @@ import java.io.Serializable;
 @Entity
 @Getter
 @NoArgsConstructor
-@TypeDef(name = "json", typeClass = JsonType.class)
 public class Magazine  implements Serializable{
-
     @Id
     @GeneratedValue( strategy = GenerationType.IDENTITY)
     private Integer id;
@@ -41,7 +37,6 @@ public class Magazine  implements Serializable{
     private String urlLink;
 
     @Builder
-
     public Magazine(Integer id, String title, String subTitle, String author, String thumbnail, Integer likes, Integer viewCount, String urlLink) {
         this.id = id;
         this.title = title;
