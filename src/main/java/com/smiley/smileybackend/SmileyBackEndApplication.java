38c4f89ae51298b7883d7fb850cfc3a867fcae3b
rlawns0327 <rlawns0327@naver.com>
2023-07-29 02:22:43 +0900

    refactor : 칫솔모 검사기능 Linux 환경 적용(#119)

--- src/main/java/com/smiley/smileybackend/SmileyBackEndApplication.java
+++ src/main/java/com/smiley/smileybackend/SmileyBackEndApplication.java
@@ -1,13 +1,19 @@
 package com.smiley.smileybackend;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class SmileyBackEndApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SmileyBackEndApplication.class, args);
+
     }
+
+
 
 }
