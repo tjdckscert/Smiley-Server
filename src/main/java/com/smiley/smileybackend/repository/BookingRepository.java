0774886ac0cce52b69e1e9713f6dc082288c7711
tjdckscert <tjdcks1051@naver.com>
2023-07-31 19:42:18 +0900

    feat : Add BookingRepository Function(#124)

--- src/main/java/com/smiley/smileybackend/repository/BookingRepository.java
+++ src/main/java/com/smiley/smileybackend/repository/BookingRepository.java
@@ -2,6 +2,7 @@ package com.smiley.smileybackend.repository;
 
 import com.smiley.smileybackend.domain.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +11,10 @@ import java.util.List;
 @Repository
 public interface BookingRepository extends JpaRepository<Booking,Integer> {
 
-    List<Booking> findByUser_Id(@Param(value = "user_id") Integer id);
+
+    @Query("select b from Booking b where b.reservDate < CURRENT_DATE and User_ID = :userid")
+    List<Booking> findPastBooking (@Param(value = "userid") Integer userID);
+
+    @Query("select b from Booking b where b.reservDate > CURRENT_DATE and User_ID = :userid")
+    Booking findPresentBooking (@Param(value = "userid") Integer userID);
 }
