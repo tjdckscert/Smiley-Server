a50d01f3a917018bc391600130218e3d8500f33c
tjdckscert <tjdcks1051@naver.com>
2023-07-31 19:41:05 +0900

    feat : Add DTO(#124)

--- src/main/java/com/smiley/smileybackend/dto/user/BookingCancelInfoDto.java
+++ src/main/java/com/smiley/smileybackend/dto/user/BookingCancelInfoDto.java
@@ -1,2 +1,20 @@
-package com.smiley.smileybackend.dto;public class BookingCancelInfoDto {
+package com.smiley.smileybackend.dto.user;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@NoArgsConstructor
+public class BookingCancelInfoDto {
+
+    @NotNull
+    @ApiModelProperty(value = "예약 번호(index), 공백 x")
+    private Integer id;
+
+    @NotNull
+    @ApiModelProperty(value = "사용자 번호(index), 공백 x")
+    private Integer userId;
 }
