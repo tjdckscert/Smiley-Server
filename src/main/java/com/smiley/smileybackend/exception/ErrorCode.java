309a0eed1221ba0cfdcf9718165f11bdfed8cb67
tjdckscert <tjdcks1051@naver.com>
2023-07-31 19:40:13 +0900

    feat : Add Error Code(#124)

--- src/main/java/com/smiley/smileybackend/exception/ErrorCode.java
+++ src/main/java/com/smiley/smileybackend/exception/ErrorCode.java
@@ -13,6 +13,8 @@ public enum ErrorCode {
     PICTURE_NOT_FOUND(NOT_FOUND, " 사진을 찾을 수 없습니다"),
     HOSPITAL_NOT_FOUND(NOT_FOUND, "병원 정보를 찾을 수 없습니다"),
     USER_NOT_FOUND(NOT_FOUND, "사용자를 찾을 수 없습니다"),
+    BOOKING_NOT_FOUND(NOT_FOUND, "예약정보를 찾을 수 없습니다."),
+    BOOKING_INFORMATION_NOT_MATCH(NOT_FOUND, "예약정보가 일치하지 않습니다."),
     MEDICINE_NOT_FOUND(NOT_FOUND, "약품 정보를 찾을 수 없습니다");
     private final HttpStatus httpStatus;
     private final String detail;
