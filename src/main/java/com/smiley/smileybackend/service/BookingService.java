32cee6872685b19d5554fb8fb9c549ddc8967da2
tjdckscert <tjdcks1051@naver.com>
2023-07-31 19:41:45 +0900

    feat : Add Service Function(#124)

--- src/main/java/com/smiley/smileybackend/service/BookingService.java
+++ src/main/java/com/smiley/smileybackend/service/BookingService.java
@@ -1,13 +1,21 @@
 package com.smiley.smileybackend.service;
 
 import com.smiley.smileybackend.domain.Booking;
+import com.smiley.smileybackend.domain.Hospital;
+import com.smiley.smileybackend.domain.User;
 import com.smiley.smileybackend.dto.response.UserBookingDto;
 import com.smiley.smileybackend.dto.response.dtolist.UserBookingDtoList;
+import com.smiley.smileybackend.dto.user.BookingCancelInfoDto;
+import com.smiley.smileybackend.dto.user.BookingInfoDto;
+import com.smiley.smileybackend.dto.user.UserMemoDto;
+import com.smiley.smileybackend.exception.ErrorCode;
+import com.smiley.smileybackend.exception.ErrorException;
 import com.smiley.smileybackend.repository.BookingRepository;
+import com.smiley.smileybackend.repository.HospitalRepository;
+import com.smiley.smileybackend.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
@@ -16,22 +24,91 @@ public class BookingService {
 
     private final BookingRepository bookingRepository;
 
-    public BookingService(BookingRepository bookingRepository) {
+    private final UserRepository userRepository;
+
+    private final HospitalRepository hospitalRepository;
+
+    public BookingService(BookingRepository bookingRepository, UserRepository userRepository, HospitalRepository hospitalRepository) {
         this.bookingRepository = bookingRepository;
+        this.userRepository = userRepository;
+        this.hospitalRepository = hospitalRepository;
     }
 
     /**
-     * 사용자의 예약 정보를 가져온다.
+     * 사용자의 과거 예약 정보를 반환 한다.
      *
      * @author : 김성찬
      * @param : 사용자 Index번호
      * @return : 예약 정보
      */    
-    public UserBookingDtoList getBookings(Integer id){
-        List<Booking> bookingDtos = bookingRepository.findByUser_Id(id);
-        return new UserBookingDtoList(bookingRepository.findByUser_Id(id)
+    public UserBookingDtoList getPastBookings(Integer id){
+        return new UserBookingDtoList(bookingRepository.findPastBooking(id)
                 .stream()
                 .map(UserBookingDto::entityToDto)
                 .collect(Collectors.toList())) ;
     }
+
+    /**
+     * 사용자의 현재 예약 정보를 반환 한다.
+     *
+     * @author : 김성찬
+     * @param : 사용자 Index번호
+     * @return : 예약 정보
+     */
+    public UserBookingDto getPresentBookings(Integer id) {
+        Booking booking = bookingRepository.findPresentBooking(id);
+        log.info(booking.toString());
+        return UserBookingDto.entityToDto(bookingRepository.findPresentBooking(id));
+    }
+
+    /**
+     * 사용자의 예약 정보를 저장 한다.
+     *
+     * @author : 김성찬
+     * @param : 사용자 예약 정보
+     * @return : 예약 정보
+     */
+    public UserBookingDto booking(BookingInfoDto bookingInfoDto) {
+        Booking booking = bookingRepository.save(bookingInfoDto.toEntity(getUser(bookingInfoDto.getUserId()),getHospital(bookingInfoDto.getHPid())));
+        return UserBookingDto.entityToDto(booking);
+    }
+
+    /**
+     * 사용자의 예약에 대한 메모를 저장한다.
+     *
+     * @author : 김성찬
+     * @param : 사용자 메모
+     * @return : 메모 정보
+     */
+    public UserBookingDto editMemo(UserMemoDto userMemoDto) {
+        Booking booking = bookingRepository.findById(userMemoDto.getId()).orElseThrow(
+                () -> new ErrorException(ErrorCode.BOOKING_NOT_FOUND)
+        );
+        booking.setMemo(userMemoDto.getMemo());
+        bookingRepository.save(booking);
+        return UserBookingDto.entityToDto(booking);
+
+    }
+    public UserBookingDto cancelBooking(BookingCancelInfoDto bookingCancelInfoDto) {
+        Booking booking = bookingRepository.findById(bookingCancelInfoDto.getId()).orElseThrow(
+                () -> new ErrorException(ErrorCode.BOOKING_NOT_FOUND)
+        );
+        if (booking.getUser().getId().equals(bookingCancelInfoDto.getUserId())) bookingRepository.delete(booking);
+        else throw new ErrorException(ErrorCode.BOOKING_INFORMATION_NOT_MATCH);
+        return UserBookingDto.entityToDto(booking);
+    }
+
+    public User getUser(Integer id){
+        return userRepository.findById(id).orElseThrow(
+                () -> new ErrorException(ErrorCode.USER_NOT_FOUND)
+        );
+    }
+
+    public Hospital getHospital(String hPid){
+        return hospitalRepository.findByHPid(hPid).orElseThrow(
+                () -> new ErrorException(ErrorCode.HOSPITAL_NOT_FOUND)
+        );
+    }
+
+
 }
