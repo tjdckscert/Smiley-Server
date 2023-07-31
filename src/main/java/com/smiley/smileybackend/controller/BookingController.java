151ac9d2efe3e8a30a2de19ae6e14f368d48b6d0
tjdckscert <tjdcks1051@naver.com>
2023-07-31 19:42:02 +0900

    feat : Add Bookingcontroller Function(#124)

--- src/main/java/com/smiley/smileybackend/controller/BookingController.java
+++ src/main/java/com/smiley/smileybackend/controller/BookingController.java
@@ -1,26 +1,63 @@
 package com.smiley.smileybackend.controller;
 
+import com.smiley.smileybackend.dto.response.UserBookingDto;
 import com.smiley.smileybackend.dto.response.dtolist.UserBookingDtoList;
+import com.smiley.smileybackend.dto.user.BookingCancelInfoDto;
+import com.smiley.smileybackend.dto.user.BookingInfoDto;
+import com.smiley.smileybackend.dto.user.UserMemoDto;
 import com.smiley.smileybackend.service.BookingService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
+@Slf4j
 @Api(tags = "Booking Controller : 예약 정보")
 public class BookingController {
 
-    private BookingService bookingService;
+    private final BookingService bookingService;
 
     public BookingController(BookingService bookingService) {
         this.bookingService = bookingService;
     }
 
-    @GetMapping("/bookings/{id}")
-    public ResponseEntity<UserBookingDtoList> bookingdtolists(@PathVariable Integer id){
-        UserBookingDtoList bookings = bookingService.getBookings(id);
+    @GetMapping("/bookings/past/{id}")
+    @ApiOperation(value="사용자 정보 과거 예약 정보" , notes = "사용자의 과거 예약 정보들을 반환한다.")
+    public ResponseEntity<UserBookingDtoList> pastBookingDtoLists(@PathVariable Integer id){
+        UserBookingDtoList bookings = bookingService.getPastBookings(id);
         return ResponseEntity.ok(bookings);
+    }
+
+    @GetMapping("/bookings/present/{id}")
+    @ApiOperation(value="사용자 정보 과거 예약 정보" , notes = "사용자의 현재 예약 정보들을 반환한다.")
+    public ResponseEntity<UserBookingDto> presentBookingDtoLists(@PathVariable Integer id){
+        UserBookingDto bookings = bookingService.getPresentBookings(id);
+        return ResponseEntity.ok(bookings);
+    }
+
+    @PostMapping("/booking")
+    @ApiOperation(value="사용자 예약" , notes = "사용자의 예약 정보를 저장한다.")
+    public ResponseEntity<UserBookingDto> booking(@Valid @RequestBody BookingInfoDto bookingInfoDto){
+        UserBookingDto userBookingInfoDto = bookingService.booking(bookingInfoDto);
+        return ResponseEntity.ok(userBookingInfoDto);
+    }
+
+    @PostMapping("/bookings/cancel")
+    @ApiOperation(value="사용자 예약 취소" , notes = "사용자의 예약을 취소한다.")
+    public ResponseEntity<UserBookingDto> cancelBooking(@Valid@RequestBody BookingCancelInfoDto bookingCancelInfoDto){
+        UserBookingDto userBookingInfoDto = bookingService.cancelBooking(bookingCancelInfoDto);
+        return ResponseEntity.ok(userBookingInfoDto);
+    }
+
+    @PostMapping("/bookings/memo")
+    @ApiOperation(value="메모 수정/저장" , notes = "사용자의 예약에 대한 메모를 수정/저장한다.")
+    public ResponseEntity<UserBookingDto> editMeno(@Valid @RequestBody UserMemoDto userMemoDto){
+        log.info(userMemoDto.toString());
+        UserBookingDto userBookingInfoDto = bookingService.editMemo(userMemoDto);
+        return ResponseEntity.ok(userBookingInfoDto);
     }
 }
